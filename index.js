a3c7e690f6dc798022b0a209aa1418293e422bbf
Wilson Herrera <wherrera3@utmachala.edu.ec>
2024-04-02 16:40:32 -0500

    firts commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+const routeBase = "/api/v1";
+const port = process.env.PORT || 3000;
+
+// Configuración CORS
+const corsOptions = {
+  origin: "*", // Reemplaza con el dominio permitido
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204,
+};
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.static("public"));
+
+// Rutas
+app.use(`${routeBase}/auth`, auth);
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado exitosamente en el puerto ${port}`);
+});
